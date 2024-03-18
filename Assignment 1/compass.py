e2e79a90467c560ebb76f8c99184ead2d146c901
Sherwin John Miranda <MIRSY007@mymail.unisa.edu.au>
2024-03-19 09:05:23 +1030

    m

--- Assignment 1/compass.py
+++ Assignment 1/compass.py
@@ -1,13 +1,13 @@
 class Compass():
-    def __init__(self,tr):
+    def __init__(self, tr):
         pass
 
     def feedback(self):
-        if player >= 7 :
+        if player >= 7:
             print("Colder")
-        
-        elif player < 7  and player > 3:
+
+        elif player < 7 and player > 3:
             print("warmer")
-        
+
         else:
             print("Hotter")
